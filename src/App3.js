1bfec862d2354b792ce5b535f1dace39b2d4a0c0
vimalceg <vimalesan.ceg@gmail.com>
2022-01-12 22:47:39 +0530

    1 file modified, 1 added

--- src/App3.js
+++ src/App3.js
@@ -1,25 +1,24 @@
 /* traditional way coding */
 import React, { useState, useEffect } from 'react';
 
-let async = () => {
-  return function Async(props) {
-    let [isLoading, setLoading] = useState(true);
-    let [Component, setComponent] = useState(null);
-    useEffect(() => {
-      import('./container/Ticket').then((res) => {
-        setLoading(false);
-        console.log('res', res);
-        setComponent(res);
-      });
-    }, []);
-    if (isLoading) {
-      return <div>JS loading...</div>;
-    } else {
-      return <Component {...props} />;
-    }
-  };
+let AsyncTicket = (props) => {
+  let [isLoading, setLoading] = useState(true);
+  let [Component, setComponent] = useState(null);
+
+  useEffect(() => {
+    import('./container/TicketOld').then((res) => {
+      // console.log('res', res.default);
+      setLoading(false);
+      setComponent(res);
+    });
+  }, []);
+  console.log('Component', isLoading, Component);
+  if (isLoading || !Component) {
+    return <div>JS loading...</div>;
+  }
+  return <div>test</div>;
 };
-let Ticket = async();
+
 export default function App() {
-  return <Ticket />;
+  return <AsyncTicket />;
 }
