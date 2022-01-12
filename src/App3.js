b7e468932120e32db58f3aa1a6247cc68f91192f
vimalceg <vimalesan.ceg@gmail.com>
2022-01-12 22:52:31 +0530

    2 files modified

--- src/App3.js
+++ src/App3.js
@@ -3,20 +3,21 @@ import React, { useState, useEffect } from 'react';
 
 let AsyncTicket = (props) => {
   let [isLoading, setLoading] = useState(true);
-  let [Component, setComponent] = useState(null);
-
+  let [ref, setComponent] = useState({});
+  let Component = ref.component;
   useEffect(() => {
     import('./container/TicketOld').then((res) => {
-      // console.log('res', res.default);
-      setLoading(false);
-      setComponent(res);
+      setTimeout(() => {
+        setLoading(false);
+        setComponent({ component: res.default });
+      }, 2000);
     });
   }, []);
   console.log('Component', isLoading, Component);
   if (isLoading || !Component) {
     return <div>JS loading...</div>;
   }
-  return <div>test</div>;
+  return <Component />;
 };
 
 export default function App() {
--- src/container/TicketOld.js
+++ src/container/TicketOld.js
@@ -1,5 +1,20 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import getTicket from '../actions/getTicket';
 export default function TicketOld() {
-  return <div>Ticket</div>;
+  let [isLoading, setLoading] = useState(true);
+  let [ticket, setTicket] = useState({});
+  useEffect(() => {
+    getTicket().then((res) => {
+      setLoading(false);
+      setTicket(res);
+    });
+  }, []);
+  if (isLoading) {
+    return <div>Data fetching...</div>;
+  }
+  return (
+    <div>
+      {ticket.id}-{ticket.subject}
+    </div>
+  );
 }
