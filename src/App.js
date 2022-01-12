ca8d7e5f7447ef0a12bf7ebca5e1ea246b2d0fbc
vimalceg <vimalesan.ceg@gmail.com>
2022-01-12 21:37:29 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import './style.css';
 
-class Suspense extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isError: false,
-    };
-  }
-  static getDerivedStateFromError(err) {
-    return {
-      isError: true,
-    };
-  }
-  componentDidMount() {
-    console.log('mount...');
-  }
-  componentDidCatch() {
-    console.log('catch...');
-    setTimeout(() => {
-      this.setState({ isError: false });
-    }, 1000);
-  }
-  render() {
-    console.log('Suspense', this.state.isError, this.props.fallback);
-    if (this.state.isError) {
-      return this.props.fallback;
-    }
-    return this.props.children;
-  }
-}
-function Loading() {
-  return <div>Loading...1</div>;
-}
+
 function wrapPromise(promise) {
   let status = 'pending';
   let result;
