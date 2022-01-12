45eb625e8cd7c58fcd1b6eae888c1b7c4d18491b
vimalceg <vimalesan.ceg@gmail.com>
2022-01-12 21:58:28 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Loading from './components/Loading';
 import wrapPromise from './utils/wrapPromise';
 import getTicket from './actions/getTicket';
 
-let getTicket = wrapPromise(getTicket);
+let getTicketW = wrapPromise(getTicket);
 function Test() {
-  let a = getTicket();
+  let a = getTicketW();
   return <div>{a}</div>;
 }
 export default function App() {
