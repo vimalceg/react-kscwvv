c85856baf8bd2e0ce8a61b579283995814d01664
vimalceg <vimalesan.ceg@gmail.com>
2022-01-12 22:58:18 +0530

    2 files modified

--- src/App4.js
+++ src/App4.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 import './style.css';
-// import Suspense from './components/Suspense';
+import Suspense from './components/Suspense';
 import Loading from './components/Loading';
-import Ticket from './container/Ticket';
+
+// import Ticket from './container/Ticket';
+// export default function App() {
+//   return (
+//     <Suspense fallback={<Loading />}>
+//       <Ticket />
+//     </Suspense>
+//   );
+// }
+
+let AsyncTicket = React.lazy(() => import('./container/Ticket'));
 export default function App() {
   return (
     <Suspense fallback={<Loading />}>
-      <Ticket />
+      <AsyncTicket />
     </Suspense>
   );
 }
