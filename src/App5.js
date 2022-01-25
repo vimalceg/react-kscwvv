647e4d853843a973ccf4c88480ac0db6e69f4a2d
vimalceg <vimalesan.ceg@gmail.com>
2022-01-25 10:07:25 +0530

    1 file modified

--- src/App5.js
+++ src/App5.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
-export default function Counter({ proCount }) {
+function Counter({ proCount }) {
   let [count, setCount] = useState(0);
   if (proCount != count) {
     setCount(proCount);
   }
   return <div>{count}</div>;
+}
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Counter proCount={10} />
+    </React.Fragment>
+  );
 }
