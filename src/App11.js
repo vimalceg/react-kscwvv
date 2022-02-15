4a510be563d76d60950a49be6fd34863a22c5922
vimalceg <vimalesan.ceg@gmail.com>
2022-02-15 10:26:03 +0530

    1 file modified

--- src/App11.js
+++ src/App11.js
@@ -1,16 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const Leaf = React.forwardRef((props, ref) => {
-  return <div ref={ref} />;
+  return <div ref={ref}>Testing</div>;
 });
 
-function SubFolder() {
-  return <Leaf />;
+const SubFolder = React.forwardRef((props, ref) => {
+  return <Leaf ref={ref} />;
+});
+
+class ClassSubFolder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.method = this.method.bind(this);
+  }
+  method() {
+    console.log(this.props);
+  }
+  render() {
+    return <div>testing</div>;
+  }
 }
 
 function Folder() {
   let ref = useRef();
+  useEffect(() => {
+    console.log(ref);
+  }, []);
   return <SubFolder ref={ref} />;
+}
+function Folder1() {
+  let ref = useRef();
+  useEffect(() => {
+    console.log(ref);
+  }, []);
+  return <ClassSubFolder ref={ref} />;
 }
 
 export default function App() {
