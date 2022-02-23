9d67b9e3c3f4a29eb8b3f9f5bddc214cb36cd925
vimalceg <vimalesan.ceg@gmail.com>
2022-02-23 16:47:21 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import App from './App10';
-import App from './event-customization/example2';
+import App from './App15';
+// import App from './event-customization/example2';
 
 ReactDOM.render(<App />, document.getElementById('root'));
